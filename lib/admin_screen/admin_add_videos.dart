fd6d126b4a0f21606ed199e6719b09fa40277208
Fawzi The-Programmer <fawz11gharib@gmail.com>
2022-02-04 11:00:02 +0300

    online class

--- lib/admin_screen/admin_add_videos.dart
+++ lib/admin_screen/admin_add_videos.dart
@@ -63,6 +63,7 @@ class AddTaskScreen extends StatelessWidget {
                   'Link': videoUrls,
                   'Name': videoTitle,
                 });
+                Navigator.pop(context);
               },
             ),
           ],
